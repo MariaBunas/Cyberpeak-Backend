881c68f41609a2b6a3bb93db9fed49728e81e2da
MariaBunas <maria.bunas@e-cdloga.ro>
2025-04-27 22:59:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "uploads/"); // Store in 'uploads' folder
+        cb(null, "./uploads/"); // Store in 'uploads' folder
     },
     filename: function (req, file, cb) {
         const customFilename = req.body.image || `image-${Date.now()}`; // Use user's filename or a default one
