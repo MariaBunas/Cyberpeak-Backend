fa0c8316c87abb12e760e2835dc42fb528a7ffa0
MariaBunas <maria.bunas@e-cdloga.ro>
2025-05-14 01:25:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,45 +17,47 @@ app.use(cors()); // Allow requests from different origins
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// service account key file from Google Cloud console.
-const KEYFILEPATH = 'ServiceAccountCred.json';
-
-// Request full drive access.
-const SCOPES = ['https://www.googleapis.com/auth/drive'];
-
-// Create a service account initialize with the service account key file and scope needed
-const auth = new google.auth.GoogleAuth({
-    keyFile: KEYFILEPATH,
-    scopes: SCOPES
-});
-
-const driveService = google.drive({version: 'v3', auth});
-
-let fileMetadata = {
-        'name': 'icon.png',
-        'parents':  [  '1lCpQoNRIPs6Q294Vt7JwDoq5GhPKEf6b'  ]
-    };
-
-let media = {
-        mimeType: 'image/jpeg',
-        body: fs.createReadStream('TestMe.png')
-    };
-
-let response = await driveService.files.create({
-    resource: fileMetadata,
-    media: media,
-    fields: 'id'
-});
-
-switch(response.status){
-    case 200:
-        let file = response.result;
-        console.log('Created File Id: ', response.data.id);
-        break;
-    default:
-        console.error('Error creating the file, ' + response.errors);
-        break;
-}
+(async () => {
+    // service account key file from Google Cloud console.
+    const KEYFILEPATH = 'ServiceAccountCred.json';
+    
+    // Request full drive access.
+    const SCOPES = ['https://www.googleapis.com/auth/drive'];
+    
+    // Create a service account initialize with the service account key file and scope needed
+    const auth = new google.auth.GoogleAuth({
+        keyFile: KEYFILEPATH,
+        scopes: SCOPES
+    });
+    
+    const driveService = google.drive({version: 'v3', auth});
+    
+    let fileMetadata = {
+            'name': 'icon.png',
+            'parents':  [  '1lCpQoNRIPs6Q294Vt7JwDoq5GhPKEf6b'  ]
+        };
+    
+    let media = {
+            mimeType: 'image/jpeg',
+            body: fs.createReadStream('TestMe.png')
+        };
+    
+    let response = await driveService.files.create({
+        resource: fileMetadata,
+        media: media,
+        fields: 'id'
+    });
+    
+    switch(response.status){
+        case 200:
+            let file = response.result;
+            console.log('Created File Id: ', response.data.id);
+            break;
+        default:
+            console.error('Error creating the file, ' + response.errors);
+            break;
+    }
+})();
 
 // 📌 Pornirea serverului pe portul 3000
 app.listen(3000, () => console.log("✅ Serverul rulează pe portul 3000"));
